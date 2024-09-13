4ad14976e2c00167882ef0d139c659982316a800
konung-yaropolk <yaropolk1995@gmail.com>
2024-09-13 18:43:27 -0500

    modified:   lanczos_tif_upscaler/lanczos_tif_upscaler.py

--- lanczos_tif_upscaler/lanczos_tif_upscaler.py
+++ lanczos_tif_upscaler/lanczos_tif_upscaler.py
@@ -4,9 +4,10 @@ import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 def upscale_tiff_stack(script_dir, scale_factor):
 
-    print('Startew dorking in the script\'s dirrectory:')
+    print('Started working in the script\'s dirrectory:')
     print(script_dir)
 
     for filename in os.listdir(script_dir):
@@ -20,15 +21,17 @@ def upscale_tiff_stack(script_dir, scale_factor):
                 frame = frame.convert("RGB")  # Ensure frame is in RGB mode
 
                 # Upscale using Lanczos algorithm
-                new_size = (int(frame.width * scale_factor), int(frame.height * scale_factor))
+                new_size = (int(frame.width * scale_factor),
+                            int(frame.height * scale_factor))
                 upscaled_frame = frame.resize(new_size, Image.LANCZOS)
                 frames.append(upscaled_frame)
 
             # Save the upscaled image
-            output_path = os.path.join(script_dir, '{}x_{}'.format(scale_factor, filename))
-            frames[0].save(output_path, save_all=True, append_images=frames[1:], compression="tiff_deflate")
+            output_path = os.path.join(
+                script_dir, '{}x_{}'.format(scale_factor, filename))
+            frames[0].save(output_path, save_all=True,
+                           append_images=frames[1:], compression="tiff_deflate")
             print(filename, 'done!')
-
 
 
 scale_factor = 1.5  # Change this to your desired scale factor
