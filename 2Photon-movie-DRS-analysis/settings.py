b3bbb2fb9f3176b0462bdfe16ea714bf39cdeacf
konung-yaropolk <yaropolk1995@gmail.com>
2025-08-21 19:44:14 -0500

    modified:   2Photon-movie-DRS-analysis/settings.py

--- 2Photon-movie-DRS-analysis/settings.py
+++ 2Photon-movie-DRS-analysis/settings.py
@@ -54,26 +54,26 @@ use_last_vertical_shift = False
 
 to_do_list = [
 
-    ['Field_1_0001_ch2_registered.tif',
-     {
-         'n_epochs': 10,
-         'stim_1_name': 'A',
-         'stim_2_name': 'C',
-         'drs_pattern': [[1, 0],  # stim#1
-                         [1, 1]]  # stim#2
-     }
-     ],
+    # ['Field_1_0001_ch2_registered.tif',
+    #  {
+    #      'n_epochs': 10,
+    #      'stim_1_name': 'A',
+    #      'stim_2_name': 'C',
+    #      'drs_pattern': [[1, 0],  # stim#1
+    #                      [1, 1]]  # stim#2
+    #  }
+    #  ],
 
-    ['Field_3_PI_0001_ch2_registered.tif',
-     {
-         'n_epochs': 10,
-         'start_from_epoch': 1,
-         'stim_1_name': 'A',
-         'stim_2_name': 'C',
-         'drs_pattern': [[1, 0],  # stim#1
-                         [1, 1]]  # stim#2
-     }
-     ],
+    # ['Field_3_PI_0001_ch2_registered.tif',
+    #  {
+    #      'n_epochs': 10,
+    #      'start_from_epoch': 1,
+    #      'stim_1_name': 'A',
+    #      'stim_2_name': 'C',
+    #      'drs_pattern': [[1, 0],  # stim#1
+    #                      [1, 1]]  # stim#2
+    #  }
+    #  ],
 ]
 
 
